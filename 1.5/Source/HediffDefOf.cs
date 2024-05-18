be5b552c1626d012dda637dd50237ffd5c81adba
Elliot Thomas <eth0net@pm.me>
2024-05-19 01:40:13 +0200

    add mod code

---
+++ 1.5/Source/HediffDefOf.cs
@@ -0,0 +1,12 @@
+﻿using RimWorld;
+using Verse;
+
+namespace InhumanizingRitual;
+
+[DefOf]
+public static class HediffDefOf
+{
+    public static HediffDef InhumanizeComa;
+
+    public static HediffDef Inhumanized;
+}
---
+++ 1.5/Source/PsychicRitualDef_Inhumanize.cs
@@ -0,0 +1,25 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+using Verse.AI.Group;
+
+namespace InhumanizingRitual;
+
+public class PsychicRitualDef_Inhumanize : PsychicRitualDef_InvocationCircle
+{
+    public SimpleCurve comaDurationDaysFromQualityCurve;
+
+    public override List<PsychicRitualToil> CreateToils(PsychicRitual psychicRitual, PsychicRitualGraph parent)
+    {
+        var list = base.CreateToils(psychicRitual, parent);
+        list.Add(new PsychicRitualToil_Inhumanize(InvokerRole, TargetRole));
+        list.Add(new PsychicRitualToil_TargetCleanup(InvokerRole, TargetRole));
+        return list;
+    }
+
+    public override TaggedString OutcomeDescription(FloatRange qualityRange, string qualityNumber, PsychicRitualRoleAssignments assignments)
+    {
+        return outcomeDescription.Formatted(Mathf.FloorToInt(comaDurationDaysFromQualityCurve.Evaluate(qualityRange.min) * 60000f).ToStringTicksToDays());
+    }
+}
